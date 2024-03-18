319c911f1cbab0be8ff72089cc3dd6bc423b92e5
Douglas Figueiredo <douglas.profi@yahoo.com>
2024-03-18 19:52:31 -0300

    Menu lateral ajustado e finalizado

--- src/app/shared/nav-bar/nav-bar.component.ts
+++ src/app/shared/nav-bar/nav-bar.component.ts
@@ -1,14 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, Renderer2, inject, ViewChild, ElementRef } from '@angular/core';
 import { CardComponent } from '../card/card.component';
 import { ButtonComponent } from '../button/button.component';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-nav-bar',
   standalone: true,
-  imports: [ButtonComponent],
+  imports: [ButtonComponent, MatIconModule],
   templateUrl: './nav-bar.component.html',
   styleUrl: './nav-bar.component.scss'
 })
 export class NavBarComponent {
+  @ViewChild('navbar', { static: false }) navbar!: ElementRef
+  renderer2 = inject(Renderer2)
 
+  openMenu() {
+    this.renderer2.removeClass(this.navbar.nativeElement, 'navbar-closed')
+  }
+  closeMenu() {
+    this.renderer2.addClass(this.navbar.nativeElement, 'navbar-closed')
+  }
 }
